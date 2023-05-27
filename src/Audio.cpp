39bc4528503206508664f21ca3293b9f3bcbde92
Rajan Patel <rpatel3001@gmail.com>
2023-05-26 22:20:34 -0400

    Update Audio.cpp

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -201,6 +201,26 @@ Audio::Audio(bool internalDAC /* = false */, uint8_t channelEnabled /* = I2S_DAC
             #else
                 m_i2s_config.communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S_MSB);
             #endif
+        
+            log_i("mode %d", m_i2s_config.mode);
+            log_i("sample_rate %d", m_i2s_config.sample_rate);
+            log_i("bits_per_sample %d", m_i2s_config.bits_per_sample);
+            log_i("channel_format %d", m_i2s_config.channel_format);
+            log_i("communication_format %d", m_i2s_config.communication_format);
+            log_i("intr_alloc_flags %d", m_i2s_config.intr_alloc_flags);
+            log_i("dma_buf_count %d", m_i2s_config.dma_buf_count);
+            log_i("dma_buf_len %d", m_i2s_config.dma_buf_len);
+            log_i("use_apll %d", m_i2s_config.use_apll);
+            log_i("tx_desc_auto_clear %d", m_i2s_config.tx_desc_auto_clear);
+            log_i("fixed_mclk %d", m_i2s_config.fixed_mclk);
+            log_i("mclk_multiple %d", m_i2s_config.mclk_multiple);
+            log_i("bits_per_chan %d", m_i2s_config.bits_per_chan);
+            log_i("chan_mask %d", m_i2s_config.chan_mask);
+            log_i("total_chan %d", m_i2s_config.total_chan);
+            log_i("left_align %d", m_i2s_config.left_align);
+            log_i("big_edin %d", m_i2s_config.big_edin);
+            log_i("bit_order_msb %d", m_i2s_config.bit_order_msb);
+            log_i("skip_msk %d", m_i2s_config.skip_msk);
 
             i2s_driver_install((i2s_port_t)m_i2s_num, &m_i2s_config, 0, NULL);
             i2s_set_dac_mode((i2s_dac_mode_t)m_f_channelEnabled);
